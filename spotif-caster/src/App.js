ca036768257a3dba2736b7197ed88ccc77bc2ff5
Rishab Rao <rishabpaws@gmail.com>
2023-05-02 22:18:29 -0500

    commits

--- spotif-caster/src/App.js
+++ spotif-caster/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+import axios from "axios";
+import { Track } from 'react-spotify-api'
+import { Search } from 'react-spotify-api'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+
+    <div className="App" style={{
+        backgroundColor: 'white',
+        width: '100%',
+        height: '100%', 
+        display: "flex", 
+        flexDirection: "column"
+    }}>   
+
+    <div style={{
+      paddingTop: 20  
+    }}>
+      Spotify Music Queuer  
+    </div>
+    
+    {/* Search bar */}
+    <div style={{
+      padding: 30
+    }}>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
+    </div>
+
+    <div>
+      <Button variant="contained">Play</Button>
+
+    </div>
+
+    <div>    
+
+    
+    </div>
+
     </div>
   );
 }
