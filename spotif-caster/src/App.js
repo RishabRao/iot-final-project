be0e7e8f2b5a39d0c11ed016782e9d22da2ff3e5
Rishab Rao <rishabpaws@gmail.com>
2023-05-07 17:43:54 -0500

    stuff

--- spotif-caster/src/App.js
+++ spotif-caster/src/App.js
@@ -4,8 +4,36 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 import axios from "axios";
-import { Track } from 'react-spotify-api'
-import { Search } from 'react-spotify-api'
+import queryString from 'query-string';
+
+
+
+  const login = () => {
+    const queryParams = queryString.stringify({
+      response_type: 'token',
+      client_id: 'ab04a22634f04a44bf5844805f4b5100',
+      redirect_uri: 'http://localhost:3000/',
+      scope: 'user-read-private user-read-email',
+    });
+    window.location = `https://accounts.spotify.com/authorize?${queryParams}`;
+  };
+
+
+const skip_song = async () => {
+
+  // const skip_url = 'https://api.spotify.com/v1/me/player/next';
+
+  // let skip_axios = axios.create(); 
+  // let data = ""
+  // const response = await skip_axios.post(skip_url, data , {
+  //   headers: { 
+  //     'Authorization': `Bearer  ${access_token}`,
+  //   }
+  // })
+
+};
+
+
 
 function App() {
   return ( 
@@ -28,14 +56,39 @@ function App() {
     <div style={{
       padding: 30
     }}>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Song" variant="outlined" />
+
+    </div>  
+
+    <div style={{
+      display: "flex", 
+      flexDirection: "row", 
+      justifyContent: "center"
+    }}>
+
+    <div style={{
+      padding: 10
+    }}>
+      <Button variant="contained" onClick={skip_song}>Play</Button>
 
     </div>
 
-    <div>
-      <Button variant="contained">Play</Button>
+    
+    <div style={{
+      padding: 10
+    }}>
+      <Button variant="contained" onClick={skip_song}>Skip</Button>
+
+    </div>  
+    <div style={{
+      padding: 10
+    }}>
+      <Button variant="contained" onClick={login}>Login</Button>
+
+    </div>  
 
     </div>
+    
 
     <div>    
 
--- spotif-caster/src/index.js
+++ spotif-caster/src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Buffer } from 'buffer';
-import { request } from 'http';
-import { SpotifyApiContext } from 'react-spotify-api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let token = "BQBOLwGcl08nFaThla04hdrDyx67c_RIs87KNL6HkYJJWkkpRfBQ8IvscslQEIpITI9g6HOOxAvhuzF0_m6634R8DyTplqj-xEYBqqzD3bM_8ufzQpME"
 
 root.render(
   <React.StrictMode>
-    <SpotifyApiContext.Provider value={token}>
     <App />
-    </SpotifyApiContext.Provider>
 
   </React.StrictMode>
 );
